a4ccda681a1efb68c642ba5bb937b8165fa0f1ba
Manisha tank <142790631+Manishatank@users.noreply.github.com>
2025-07-09 08:26:05 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,6 @@
+/*old javascript does not contain class modules and arrowfunctions */
+// if you write "use Strict" all your javascript code treat as a new version of javascript or as a new javascript code
+// alert(3+3) it is not give an alert because we are using nodejs not browser in browser we can run this but in node js different method to write alert
+// coderelability should be high
+// Ecma script standards of javascript how to write javascript
+// tc39 ecma script and documentation
