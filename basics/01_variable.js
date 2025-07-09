6fca0c30101c375f2afa9ec3b0a99a4b7eaaa095
Manisha tank <142790631+Manishatank@users.noreply.github.com>
2025-07-09 07:21:11 +0000

    new file

---
+++ basics/01_variable.js
@@ -0,0 +1,10 @@
+// in this we learn and read about variable 
+// variable is a container that store or hold value in computer memory variable is a value that can change depending on the situation and condition
+// const variable if we define variable one time and no changes
+// how variable can declare
+
+const accountId = 12345;
+let account = "myname";
+var accountEmail = "tankmanisha05@gmail.com";
+accountId = 3456;
+console.log(accountId);
