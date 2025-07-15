2528f14fc9c7fd410279a7dff839a4104075bcd1
Manisha tank <142790631+Manishatank@users.noreply.github.com>
2025-07-15 06:17:25 +0000

    string and numberrs

---
+++ basics/05_string.js
@@ -0,0 +1,39 @@
+// string interpolation is a feature of many programming language that allows you to embeded variables and expression directly into strings 
+// It is a new method to concatinate variable and expressions in string directly 
+let name = "manisha";
+let num = 10;
+console.log(`hello my name ${name} this is me ${num}`);
+// // old method to concatinate string 
+let now = "man";
+let flex = 10;
+console.log(now+flex);
+// string is key value pair it is an object not an array 
+// its second method how to define string 
+let my = new String("kajjd");
+console.log(my);
+// value pair access
+console.log(my[2]);
+// methhods of string 
+// String length
+// String charAt()
+// String charCodeAt()
+// String codePointAt()
+// String concat()
+// String at()
+// String [ ]
+// String slice()
+// String substring()
+// String substr()
+// String toUpperCase()
+// String toLowerCase()
+// String isWellFormed()
+// String toWellFormed()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String repeat()
+// String replace()
+// String replaceAll()
+// String split()
