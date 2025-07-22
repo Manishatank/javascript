c662ab66b1f1dd911ba84b7cff2ed55b9716f04f
Manisha tank <142790631+Manishatank@users.noreply.github.com>
2025-07-22 14:38:15 +0000

    pracrtice

---
+++ basics/03_control_flow/truthy.js
@@ -0,0 +1,14 @@
+// if we assume string value is truthy 
+const userEmail = "manisha"
+if (userEmail){
+    console.log("its is the true value")
+}else{
+    console.log("truthy")
+}
+// agr koi value ni hoti toh truthy print hota 
+// value hai toh its is the true value print hoga 
+
+// ++++++++++++++truthy true and falsy value +++++++++++++++++++
+// falsy value 
+// false , 0, -0,Nan,null , undefined, BigInt 0n , ""
+// truthy value 
---
+++ practice.js
@@ -0,0 +1,33 @@
+// Reverse a string
+
+// string ka reverse method ni hota hai toh hume pehle string ko array m convert karna padega
+// split use for convert string to array
+// let my = "hello";
+// let arr = my.split('');
+// console.log(arr);
+// // if we convert array back to string use join
+// let name = arr.join('');
+// console.log(name);
+// // program of reverse a string is use two methods
+// // convert string using split method ('')
+// // reverse a array using reverse method 
+// // again convert string using join method
+
+// let myWorld = "new name";
+// let myname = myWorld.split(''); use convert string to array
+// let mynephew = myname.reverse(''); use revrse an array
+// // let myman = mynephew.join('');use convert array to string
+// console.log(myman);
+
+// using a function
+//  function string(str){
+//     return str.split('').reverse('').join('');
+    
+
+//  }
+//  console.log(string("hello"));
+
+
+
+// check if string is palindrome 
+// palindrome means if number string is reverse is equal
